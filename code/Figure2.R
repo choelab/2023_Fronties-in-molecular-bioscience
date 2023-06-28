672107cde23063a29f8dee239eb1d5904aad55dd
Korea Brain Research Institute <122330748+choelab@users.noreply.github.com>
2023-06-28 11:20:55 +0900

    Update Figure2.R

--- code/Figure2.R
+++ code/Figure2.R
@@ -5,7 +5,11 @@ options(stringsAsFactors = FALSE)
 brain_cells.subset <- readRDS("/choelab/Fronties-in-molecular-bioscience-2023/data/brain_cells_merged_including_06m_2023-06-16 23-39-03.RDS")
 Idents(object = brain_cells.subset) <- brain_cells.subset@meta.data$SCT_snn_res.0.8
 
-mg.subsetted <- subset(brain_cells.subset, subset = platform =="inDrops", ident = 13)
+brain_cells.markers <- FindAllMarkers(brain_cells.subset,
+                                    only.pos = TRUE, logfc.threshold = 1, 
+                                    assay = "RNA")
+
+mg.subsetted <- subset(brain_cells.subset, ident = 13)
 
 ###### Dimension reduction and clustering
 pcs <- 1:50
@@ -13,10 +17,9 @@ mg.subset <- mg.subsetted %>%
                  RunPCA(verbose = FALSE, features = VariableFeatures(mg.subsetted)) %>%
                  RunUMAP(reduction = "pca", umap.method = "umap-learn", dims=pcs) %>%
                  FindNeighbors(reduction = "pca", dims=pcs) %>%
-                 FindClusters(resolution =seq(0.1,2,0.05), algorithm = 2)
+                 FindClusters(resolution =1.8, algorithm = 2)
 
 Idents(object = mg.subset) <- sprintf("%02d",mg.subset@meta.data$SCT_snn_res.1.8)
-
 mg.subset@meta.data$cell_type_age[colnames(mg.subset) %in% WhichCells(subset(mg.subset, ident = '01'))] <- "MG-01"
 mg.subset@meta.data$cell_type_age[colnames(mg.subset) %in% WhichCells(subset(mg.subset, ident = '02'))] <- "MG-02"
 mg.subset@meta.data$cell_type_age[colnames(mg.subset) %in% WhichCells(subset(mg.subset, ident = '03'))] <- "MG-03"
@@ -32,8 +35,8 @@ mg.subset@meta.data$cell_type_age[colnames(mg.subset) %in% WhichCells(subset(mg.
 
 ###### single-cell RNA inference analysis using monocle3
 
-library(SeuratWrappers)
-library(monocle3)
+require(SeuratWrappers)
+require(monocle3)
 
 get_correct_root_state <- function(cds, cell_phenotype, root_type){
   cell_ids <- which(pData(cds)[, cell_phenotype] == root_type)
@@ -134,7 +137,7 @@ cds_subset <- lapply(cds_sub, function(cds.cell){
 
 counts <- exprs(cds)
 
-library(tradeSeq)
+require(tradeSeq)
 sces <- fitGAM(counts = counts,
               pseudotime = pseudotime,
               cellWeights = cellWeights)
@@ -152,9 +155,23 @@ gene.p <- data.frame(patternRes) %>% dplyr::filter(pvalue_4vs5 < 0.01) %>% rowna
 gene.4 <- data.frame(assoRes) %>% dplyr::filter(pvalue_4 < 0.01) %>% rownames()
 gene.5 <- data.frame(assoRes) %>% dplyr::filter(pvalue_5 < 0.01) %>% rownames()
 
+insGenes <- intersect(gene.p[!grepl("Rik$|^Gm|^mt.|n.", gene.p)],intersect(gene.4, gene.5))
+
+cds_subset.5 <- cds[,rownames(colData(cds)) %in% cds_sub[[5]]$cells]
+cds_subset.4 <- cds[,rownames(colData(cds)) %in% cds_sub[[4]]$cells]
+
+pt.matrix.5 <- exprs(cds_subset.5)[match(insGenes,rownames(rowData(cds_subset.5))),order(pseudotime(cds_subset.5))]
+pt.matrix.4 <- exprs(cds_subset.4)[match(insGenes,rownames(rowData(cds_subset.4))),order(pseudotime(cds_subset.4))]
+
+pt.matrix.5 <- t(apply(pt.matrix.5, 1,function(x){smooth.spline(x,df=3)$y}))
+pt.matrix.5 <- t(apply(pt.matrix.5, 1,function(x){(x-mean(x))/sd(x)}))
+rownames(pt.matrix.5) <- insGenes;
+
+pt.matrix.4 <- t(apply(pt.matrix.4, 1,function(x){smooth.spline(x,df=3)$y}))
+pt.matrix.4 <- t(apply(pt.matrix.4, 1,function(x){(x-mean(x))/sd(x)}))
+rownames(pt.matrix.4) <- insGenes;
 
 ###### Figure 2A
-
 require(scales)
 Figure2A_total<-DimPlot(brain_cells.subset, 
               # group.by = "age", 
@@ -187,8 +204,7 @@ Figure2A_WT<-DimPlot(subset(mg.subset, subset = genetype ==names(table(mg.subset
         axis.text = element_blank(),  axis.ticks = element_blank()) + 
   ggtitle(paste0("WT - ",comma(length(colnames(subset(mg.subset, subset = genetype ==names(table(mg.subset$genetype))[2]))),format ="d")," cells")) + NoAxes()
 
-
-Figure2A_5xFAD<-DimPlot(subset(mg.subset, subset = genetype ==names(table(mg.subset$genetype))[1]), 
+Figure2A_5xFAD <- DimPlot(subset(mg.subset, subset = genetype ==names(table(mg.subset$genetype))[1]), 
   group.by = "cell_type_age", 
   label =T, label.size = 5, pt.size = 1,raster=FALSE)+
   theme(axis.title = element_blank(), 
@@ -197,10 +213,9 @@ Figure2A_5xFAD<-DimPlot(subset(mg.subset, subset = genetype ==names(table(mg.sub
         axis.text = element_blank(),  axis.ticks = element_blank()) + 
   ggtitle(paste0("5xFAD - ",comma(length(colnames(subset(mg.subset, subset = genetype ==names(table(mg.subset$genetype))[1]))),format ="d")," cells")) + NoAxes()
 
-
 ###### Figure 2B
 require(Nebulosa)
-Figure2B<-plot_density(mg.subset, 
+Figure2B <- plot_density(mg.subset, 
               features = c("Tmem119","P2ry12","Cx3cr1",
                           "Apoe","Trem2","Cst7", 
                           "Axl","Dkk2","Hif1a"), reduction = "umap" ,
@@ -208,7 +223,72 @@ Figure2B<-plot_density(mg.subset,
                method = c("wkde"), size = 1, adjust=1)
 
 ###### Figure 2C
-
+Figure2C_routeA <-plot_cells(cds_subset[[5]],
+               color_cells_by = "pseudotime",
+               show_trajectory_graph = TRUE,
+               trajectory_graph_color = "grey50",
+               trajectory_graph_segment_size = 0.25,
+               label_cell_groups=FALSE,
+               label_roots = TRUE,
+               label_leaves=TRUE,
+               label_branch_points=FALSE,
+               graph_label_size = 2,
+               cell_size = 0.5)
+Figure2C_routeB <- plot_cells(cds_subset[[4]],
+               color_cells_by = "pseudotime",
+               show_trajectory_graph = TRUE,
+               trajectory_graph_color = "grey50",
+               trajectory_graph_segment_size = 0.25,
+               label_cell_groups=FALSE,
+               label_roots = TRUE,
+               label_leaves=TRUE,
+               label_branch_points=FALSE,
+               graph_label_size = 2,
+               cell_size = 0.5)
 
 ###### Figure 2D
-plotSmoothers(sces, counts, gene = c("Trem2"), lwd = 1, nPoints = 100, lineagesToPlot = c(4,5))
+require(ggvenn)
+list_venn <- list("Track A" = gene.5,
+                  "Track B" = gene.4)
+
+Figure2D <- ggvenn(list_venn, c("Track A", "Track B"))  
+
+###### Figure 2E
+Figure2E <- plotSmoothers(sces, counts, gene = c("Trem2"), lwd = 1, nPoints = 100, lineagesToPlot = c(4,5))
+
+###### Figure 2F
+hthc.5 <- Heatmap(
+  pt.matrix.5,
+  name                         = "z-score",
+  # col                          = colorRamp2(seq(from=-2,to=2,length=11),rev(brewer.pal(11, "Spectral"))),
+  show_row_names               = TRUE,
+  show_column_names            = FALSE,
+  # row_labels = ifelse(rownames(pt.matrix) %in% as.character(convert_human_to_mouse_symbols(unique(unlist(str_split(enrich_result.sel$G,"\\;"))))), rownames(pt.matrix), ""),
+  # row_labels = ifelse(rownames(pt.matrix) %in% c("Trem2",gene.4s), rownames(pt.matrix), ""),
+  row_names_gp                 = gpar(fontsize = 8),
+  km = 5,
+  # clustering_method_rows = "ward.D2",
+  # clustering_method_columns = "ward.D2",
+  row_title_rot                = 0,
+  cluster_rows                 = TRUE,
+  cluster_row_slices           = FALSE,
+  cluster_columns              = FALSE)
+
+hthc.4 <- Heatmap(
+  pt.matrix.4,
+  name                         = "z-score",
+  # col                          = colorRamp2(seq(from=-2,to=2,length=11),rev(brewer.pal(11, "Spectral"))),
+  show_row_names               = TRUE,
+  show_column_names            = FALSE,
+  # row_labels = ifelse(rownames(pt.matrix.4) %in% as.character(convert_human_to_mouse_symbols(unique(unlist(str_split(enrich_result.sel4$Genes,"\\;"))))), rownames(pt.matrix.4), ""),
+  # row_labels = ifelse(rownames(pt.matrix.4) %in% c("Cyld","Tacr1","Tbc1d24","Lrba"), rownames(pt.matrix.4), ""),
+  row_names_gp                 = gpar(fontsize = 8),
+  km = 4,
+  # clustering_method_rows = "ward.D2",
+  # clustering_method_columns = "ward.D2",
+  row_title_rot                = 0,
+  cluster_rows                 = TRUE,
+  cluster_row_slices           = FALSE,
+  cluster_columns              = FALSE)
+
+Figure2F <-  hthc.5 + hthc.4
